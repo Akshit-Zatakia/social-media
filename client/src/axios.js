0e4498d3ddcfa21d7cbd48e3f59cdc307b7dc54c
Akshit-Zatakia <akshitzatakia0@gmail.com>
2021-09-29 19:39:18 +0530

    server url changed

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const baseURL = "http://localhost:8081/api/";
+const prodURL = "https://social-media-0.herokuapp.com/api/";
 
 const instance = axios.create({
-  baseURL,
+  baseURL: prodURL,
   headers: {
     "Content-type": "application/json",
   },
